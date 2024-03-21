41144fd3d4260d173cf3df26ace1c33d0fdbc3b7
Mateusz Słotwiński <mateuszslotwinski16@gmail.com>
2024-03-21 13:14:51 +0100

    cleanings

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -6,8 +6,6 @@ const routes: RouteRecordRaw[] = [
     component: () => import('pages/Index.vue'),
   },
 
-  // Always leave this as last one,
-  // but you can also remove it
   {
     path: '/:catchAll(.*)*',
     component: () => import('pages/ErrorNotFound.vue'),
